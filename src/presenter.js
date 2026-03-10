fd70a741d501af1c2262eb4b434518d1c6106763
Santiago Torres <santiago.torres@ucb.edu.bo>
2026-03-09 20:48:30 -0400

    feat: mostrar símbolo de dólar en los valores monetarios del totalizador

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import { cantItem,precioItem,precioNeto,impuestoEstado, calcularPrecioConImpuesto, calcularImpuesto } from "./totalizador.js";
+import { cantItem,precioItem,precioNeto,impuestoEstado, calcularPrecioConImpuesto, calcularImpuesto, calcularPrecioConImpuestoYDescuento } from "./totalizador.js";
 
 const form = document.querySelector("#totalizar-form");
 
@@ -21,12 +21,13 @@ form.addEventListener("submit", (event) => {
   const impuesto = impuestoEstado(estado);
   const impuestoMonetario = calcularImpuesto(neto,impuesto);
   const precioTotalImpuesto = calcularPrecioConImpuesto(estado,cantidad,precio);
+  const precioFinal = calcularPrecioConImpuestoYDescuento(estado,cantidad,precio);
 
   resultado.innerHTML =
     "<p>Cantidad de items: " + cant + "</p>" +
-    "<p>Precio por item: " + prec + "</p>" +
+    "<p>Precio por item: $" + prec + "</p>" +
     "<p>Codigo de estado: " + estado + "</p>" +
-    "<p>Precio neto (" + cant + "*" + prec + "): " + neto + "</p>" +
-    "<p>Impuesto para " + estado + "(%" + impuesto + "): " + impuestoMonetario + "</p>" +
-    "<p>Precio total (+impuesto): " + precioTotalImpuesto + "</p>";
+    "<p>Precio neto (" + cant + "*$" + prec + "): $" + neto + "</p>" +
+    "<p>Impuesto para " + estado + "(%" + impuesto + "): $" + impuestoMonetario + "</p>" +
+    "<p>Precio total (descuento e impuesto): $" + precioFinal + "</p>";
 });
